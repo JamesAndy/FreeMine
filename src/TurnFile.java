b52545d3498e61f1d306a1d255058b6b9d898447
JamesChang <james89926282@gmail.com>
2020-03-21 12:37:47 +0800

    整理File功能

--- src/TurnFile.java
+++ src/TurnFile.java
@@ -1,17 +1,13 @@
 /**
- *  Base on JDK : Oracle JDK 1.8
+ *  Base on JDK : OpenJDK 14
  *  Creat By : JamesChang
  *  Creat Time: 2020-03-20 11:41
  *  File Description: 純文字檔轉碼
  */
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 import static java.lang.System.out;
 
-import org.mozilla.universalchardet.UniversalDetector;
+import com.jc.file.JcFile;
 
 public class TurnFile {
 
@@ -21,16 +17,15 @@ public class TurnFile {
     //參數設定:設定轉碼目標
     private final static String trunEncode = "UTF-8";
     //參數設定:起始目錄
-    private final static String startPath = "D:/Wezoomtek/ntpc/swjweb/web/jsp";
+    private final static String startPath = "D:/Wezoomtek/ntpc/swjweb/src";
     //參數設定:篩選檔案
-    private final static String pFilter = ".jsp";
+    private final static String pFilter = ".properties";
     //參數設定:取代路徑
-    private final static String replacePath = "D:/Wezoomtek/ntpc/swjweb/web/";
+    private final static String replacePath = "D:/Wezoomtek/ntpc/swjweb/";
     //參數設定:目的路徑
-    private final static String purposePath = "E:/Desktop/replace1";
+    private final static String purposePath = "E:/Desktop/swjweb/src";
 
-    /** 檔案集合 */
-    private static List<String> gFileList = new ArrayList<String>();
+
 
     /**
      * 執行主程式
@@ -41,18 +36,31 @@ public class TurnFile {
 
 
         //取得要轉檔的檔案路徑
-        getPathDir(startPath,pFilter);
+        JcFile.getPathDir(startPath,pFilter);
 
         //執行
-        for( String fileSourceDir : gFileList ){
+        for( String fileSourceDir : JcFile.gFileList ){
 
             String goalFileDir = fileSourceDir.replace(replacePath,purposePath);
 
-            String code = checkEncoding(fileSourceDir);
+            String code = JcFile.checkEncoding(fileSourceDir);
             if(code != null){
-                creatFile(goalFileDir);
+//                if(
+//                    !searchFileContent(fileSourceDir,code,"<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">")
+//                        &&
+//                    searchFileContent(fileSourceDir,code,"<head>")
+//                ){
+//                    out.println(fileSourceDir);
+//                    creatFile(goalFileDir);
+//                    //寫入檔案內容
+//                    convertFileCode(fileSourceDir,goalFileDir,code);
+//                    trunFiles ++;
+//                }
+                out.println(fileSourceDir);
+                out.println(goalFileDir);
+                JcFile.creatFile(goalFileDir);
                 //寫入檔案內容
-                convertFileCode(fileSourceDir,goalFileDir,code);
+                JcFile.convertFileCode(fileSourceDir,goalFileDir,code,trunEncode);
                 trunFiles ++;
             }
             else{
@@ -62,124 +70,5 @@ public class TurnFile {
         }
 
         out.println("共計轉換 "+trunFiles+" 檔案");
-    }
-
-
-    /**
-     * 建立檔案
-     * @param path
-     * @throws IOException
-     */
-    private static void creatFile(String path) throws IOException {
-        String checkPath = path.substring(0,path.lastIndexOf("/"));
-        File newPath = new File( checkPath );
-        //判斷目錄是否存在，不存在則建立
-        if(!newPath.exists()){
-            newPath.mkdirs();
-        }
-        //判斷檔案是否存在，不存在則建立
-        File newFile = new File( path );
-        if(!newFile.exists()){
-            newFile.createNewFile();
-        }
-    }
-
-    /**
-     * 取得要處理的檔案
-     * @param startPath
-     * @param fileFilter
-     */
-    private static void getPathDir(String startPath,String fileFilter){
-        File topPathFile = new File( startPath );
-        String[] filePathArray = topPathFile.list();
-        if(filePathArray != null){
-            for(int i=0 ; i< filePathArray.length ; i++){
-                String filePath = startPath+"/"+ filePathArray[i];
-                File file = new File( filePath);
-                //判斷是否為檔案
-                if(file.isFile()){
-                    if( fileFilter.equals("*") || file.getPath().indexOf(fileFilter) >= 0 )
-                        gFileList.add( startPath + "/" + filePathArray[i] );
-                }
-                else {
-                    getPathDir(filePath,fileFilter);
-                }
-            }
-        }
-        else {
-            gFileList.add( startPath );
-        }
-
-    }
-
-
-    /**
-     * 轉碼程式
-     * @param fileSourceDir
-     * @param goalFileDir
-     * @throws IOException
-     */
-    private static void convertFileCode(String fileSourceDir, String goalFileDir,String code) throws IOException {
-
-        BufferedReader br = new BufferedReader( new InputStreamReader(new FileInputStream(fileSourceDir), code));
-
-        PrintWriter pw = new PrintWriter( new OutputStreamWriter(new FileOutputStream(goalFileDir), trunEncode));
-
-        String line = br.readLine();
-
-        //判斷有執行取代的資料1
-        boolean hadReplace = false;
-
-        while (line != null) {
-
-            //取代資料
-            if(line.toUpperCase().indexOf("BIG5") != -1){
-                line = line.replace("big5","UTF-8");
-                line = line.replace("BIG5","UTF-8");
-                line = line.replace("Big5","UTF-8");
-                line = line.replace("bIg5","UTF-8");
-                line = line.replace("biG5","UTF-8");
-                hadReplace = true;
-            }
-            else if(line.toUpperCase().indexOf("UTF-8") != -1){
-                hadReplace = true;
-            }
-
-            pw.write(line+"\r\n");
-            line = br.readLine();
-        }
-        if(!hadReplace){
-            out.println("已轉碼，需人工檢視檔案："+fileSourceDir);
-        }
-
-        pw.flush();
-        pw.close();
-
-    }
-
-    /**
-     * 確認檔案編碼
-     * @param filePath 檔案路徑
-     * @return [String]charset:編碼格式
-     * @throws IOException
-     */
-    private static String checkEncoding(String filePath) throws IOException {
-
-        /* 讀取檔案 */
-        FileInputStream fis = new FileInputStream(filePath);
-        /* 建立分析器 */
-        UniversalDetector detector = new UniversalDetector(null);
-
-        int nread;
-        byte[] buf = new byte[4];
-        while ((nread = fis.read(buf)) > 0 && !detector.isDone()) {
-            /* 分析資料 */
-            detector.handleData(buf, 0, nread);
-        }
-        fis.close();
-        detector.dataEnd();
-        /* 取得編碼格式 */
-        String charset = detector.getDetectedCharset();
-        return charset;
     }
 }
---
+++ src/com/jc/file/JcFile.java
@@ -0,0 +1,166 @@
+/**
+ *  Base on JDK : OpenJDK 14
+ *  Creat By : JamesChang
+ *  Creat Time: 2020-03-20111:41
+ *  File Description: 檔案處理
+ */
+package com.jc.file;
+
+
+import org.mozilla.universalchardet.UniversalDetector;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JcFile {
+
+
+    /** 檔案集合 */
+    public static List<String> gFileList = new ArrayList<String>();
+
+    /**
+     * 建立檔案
+     * @param path
+     * @throws IOException
+     */
+    public static void creatFile(String path) throws IOException {
+        String checkPath = path.substring(0,path.lastIndexOf("/"));
+        java.io.File newPath = new java.io.File( checkPath );
+        //判斷目錄是否存在，不存在則建立
+        if(!newPath.exists()){
+            newPath.mkdirs();
+        }
+        //判斷檔案是否存在，不存在則建立
+        java.io.File newFile = new java.io.File( path );
+        if(!newFile.exists()){
+            newFile.createNewFile();
+        }
+    }
+
+    /**
+     * 取得要處理的檔案
+     * @param startPath
+     * @param fileFilter
+     */
+    public static void getPathDir(String startPath,String fileFilter){
+        File topPathFile = new File( startPath );
+        String[] filePathArray = topPathFile.list();
+        if(filePathArray != null){
+            for(int i=0 ; i< filePathArray.length ; i++){
+                String filePath = startPath+"/"+ filePathArray[i];
+                File file = new File( filePath);
+                //判斷是否為檔案
+                if(file.isFile()){
+                    if( fileFilter.equals("*") || file.getPath().indexOf(fileFilter) >= 0 )
+                        gFileList.add( startPath + "/" + filePathArray[i] );
+                }
+                else {
+                    getPathDir(filePath,fileFilter);
+                }
+            }
+        }
+        else {
+            gFileList.add( startPath );
+        }
+
+    }
+
+
+    /**
+     * 確認檔案編碼
+     * @param filePath 檔案路徑
+     * @return [String]charset:編碼格式
+     * @throws IOException
+     */
+    public static String checkEncoding(String filePath) throws IOException {
+
+        /* 讀取檔案 */
+        FileInputStream fis = new FileInputStream(filePath);
+        /* 建立分析器 */
+        UniversalDetector detector = new UniversalDetector(null);
+
+        int nread;
+        byte[] buf = new byte[4];
+        while ((nread = fis.read(buf)) > 0 && !detector.isDone()) {
+            /* 分析資料 */
+            detector.handleData(buf, 0, nread);
+        }
+        fis.close();
+        detector.dataEnd();
+        /* 取得編碼格式 */
+        String charset = detector.getDetectedCharset();
+
+        /*預設ANSI*/
+        if(charset == null){
+            charset = "Cp1252";
+        }
+
+        return charset;
+    }
+
+
+    /**
+     * 尋找資料，模糊查詢
+     * @param filePath 目標檔案路徑
+     * @param fileCode 檔案編碼
+     * @param keyWord 查詢關鍵字
+     * @return 有找到
+     * @throws IOException
+     */
+    public static boolean searchFileContent(String filePath,String fileCode, String keyWord) throws Exception {
+
+        if(filePath == null){throw new Exception("The function searchFileContent param [filePath] can't null ");}
+        if(fileCode == null){throw new Exception("The function searchFileContent param [fileCode] can't null ");}
+        if(keyWord == null){throw new Exception("The function searchFileContent param [keyWord] can't null ");}
+
+        keyWord = keyWord.toUpperCase();
+        keyWord = keyWord.replaceAll("\\s*", "");
+
+        boolean isFind = false;
+
+        BufferedReader br = new BufferedReader( new InputStreamReader(new FileInputStream(filePath), fileCode));
+
+        String line = br.readLine();
+        while (line != null) {
+
+            line = line.toUpperCase();
+            line = line.replaceAll("\\s*", "");
+
+            if(line.indexOf(keyWord.toUpperCase()) != -1){
+                isFind = true;
+                break;
+            }
+
+            line = br.readLine();
+        }
+        return isFind;
+    }
+
+    /**
+     * 轉碼程式
+     * @param fileSourceDir 檔案來源
+     * @param goalFileDir 存放檔案目標路徑
+     * @param code 原檔編碼
+     * @param trunEncode 目標編碼
+     * @throws IOException
+     */
+    public static void convertFileCode(String fileSourceDir, String goalFileDir,String code,String trunEncode) throws IOException {
+
+        BufferedReader br = new BufferedReader( new InputStreamReader(new FileInputStream(fileSourceDir), code));
+
+        PrintWriter pw = new PrintWriter( new OutputStreamWriter(new FileOutputStream(goalFileDir), trunEncode));
+
+        String line = br.readLine();
+
+        while (line != null) {
+            pw.write(line+"\r\n");
+            line = br.readLine();
+        }
+
+        pw.flush();
+        pw.close();
+
+    }
+}
