23b1239efa533f6d0d43d867583a51ead54d1ac8
JamesChang <james89926282@gmail.com>
2020-03-28 12:15:23 +0800

    更新File操作方式

--- src/TurnFile.java
+++ src/TurnFile.java
@@ -17,13 +17,13 @@ public class TurnFile {
     //參數設定:設定轉碼目標
     private final static String trunEncode = "UTF-8";
     //參數設定:起始目錄
-    private final static String startPath = "D:/Wezoomtek/ntpc/swjweb/src";
+    private final static String startPath = "D:/Wezoomtek/ntpc/swjweb/";
     //參數設定:篩選檔案
-    private final static String pFilter = ".properties";
+    private final static String pFilter = ".java";
     //參數設定:取代路徑
     private final static String replacePath = "D:/Wezoomtek/ntpc/swjweb/";
     //參數設定:目的路徑
-    private final static String purposePath = "E:/Desktop/swjweb/src";
+    private final static String purposePath = "E:/Desktop/swjweb/jsp";
 
 
 
@@ -56,12 +56,19 @@ public class TurnFile {
 //                    convertFileCode(fileSourceDir,goalFileDir,code);
 //                    trunFiles ++;
 //                }
-                out.println(fileSourceDir);
-                out.println(goalFileDir);
-                JcFile.creatFile(goalFileDir);
-                //寫入檔案內容
-                JcFile.convertFileCode(fileSourceDir,goalFileDir,code,trunEncode);
-                trunFiles ++;
+//                out.println(fileSourceDir);
+//                out.println(goalFileDir);
+//                JcFile.creatFile(goalFileDir);
+//                //寫入檔案內容
+//                JcFile.convertFileCode(fileSourceDir,goalFileDir,code,trunEncode);
+
+
+                //僅搜尋檔案內容
+                if(JcFile.searchFileContent(fileSourceDir,code,"sun.")){
+                    out.println(fileSourceDir);
+                    trunFiles ++;
+                }
+
             }
             else{
                 out.println("未轉碼檔案："+fileSourceDir);
