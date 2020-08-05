c81b55b6a599c342afc57684675198427fb6f93d
JamesChang <james89926282@gmail.com>
2020-08-05 09:11:16 +0800

    feat: 新增test
    調整項目:
    
    調正程式：

---
+++ Groovy/src/test.groovy
@@ -0,0 +1,8 @@
+
+/**
+ * File Description/檔案描述:
+ * @author JamesChang* @since 2020/7/26上午 06:48
+ * @version 1.0* */
+
+String test = ""
+assert test, "Cannot load Requestmaps; 'requestMap.className' property is not specified"
